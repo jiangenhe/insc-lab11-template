bf938cbe16ac835928648755fc40f83808fe311c
jiangenhe <jiangenhe@gmail.com>
2021-04-06 14:30:50 -0400

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,67 @@
+d3.csv('data.csv')
+  .then(data => {
+    // Convert sales strings to numbers
+    data.forEach(d => {
+      d.sales = +d.sales;
+    });
+    console.log(data)
+
+    showBarChart(data);
+  })
+  .catch(error => {
+    console.error('Error loading the data');
+  });
+
+/*
+ *  Draw chart
+ */
+function showBarChart(data) {
+  const width = 500;
+  const height = 120;
+  const marginBottom = 50
+  const marginLeft = 50;
+
+  // Append empty SVG container and set size
+  const svg = d3.select('#chart').append('svg')
+    .attr('width', width + marginLeft)
+    .attr('height', height + marginBottom);
+
+  // Initialize linear and ordinal scales (input domain and output range)
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d => d.sales)])
+    .range([0, width ]);
+
+  const yScale = d3.scaleBand()
+    .domain(data.map(d => d.month))
+    .range([0, height])
+    .paddingInner(0.1);
+
+  // Initialize axes
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale);
+
+  const xAxisGroup = svg.append('g')
+    .attr('class', 'axis x-axis')
+    .attr('transform', `translate(${marginLeft}, ${height})`)
+    .call(xAxis);
+
+  const yAxisGroup = svg.append('g')
+    .attr('class', 'axis y-axis')
+    .attr('transform', `translate(${marginLeft}, 0)`)
+    .call(yAxis);
+
+  const barGroup = svg.append('g')
+    .attr('transform', `translate(${marginLeft},0)`)
+  // Add rectangles
+  const bars = barGroup
+    .selectAll('rect')
+    .data(data)
+    .join('rect')
+    .attr('class', 'bar')
+    .attr('fill', 'steelblue')
+    .attr('width', d => xScale(d.sales))
+    .attr('height', yScale.bandwidth())
+    .attr('y', d => yScale(d.month))
+    .attr('x', 0);
+
+}
